a4b08e69018da9b6e93bb7b341f2b120da59ff06
stetteh <stettehq@gmail.com>
2016-04-07 19:45:02 -0500

    Blog Post CLass

---
+++ Blog/Models/BlogPost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime DateCreated { get; set; }
+
+    }
+}
