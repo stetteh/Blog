9b9cf9e6109795d966501b050b33ca2d412c46d3
stetteh <stettehq@gmail.com>
2016-04-07 20:28:46 -0500

    migration and update database

---
+++ Blog/Migrations/201604080114167_initalblogmigration.cs
@@ -0,0 +1,29 @@
+namespace Blog.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initalblogmigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BlogPosts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Author = c.String(),
+                        Title = c.String(),
+                        Body = c.String(),
+                        DateCreated = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.BlogPosts");
+        }
+    }
+}
